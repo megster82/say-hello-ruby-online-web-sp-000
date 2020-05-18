dbea479a252cb7c671c728899918691041e20255
meaghan jennings <meaghanljennings@gmail.com>
2020-05-18 21:53:08 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+#define say_hello method that defaults to Ruby Programmer when name is passed in
+
+def say_hello(name)
+  puts "Hello(name)!"
+end
+
+def say_hello (name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
