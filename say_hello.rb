d1f81b6b167218c86355fc5edb7e5c7e659d7761
meaghan jennings <meaghanljennings@gmail.com>
2021-02-02 17:08:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,4 @@
 #define say_hello method that defaults to Ruby Programmer when name is passed in
-
-def say_hello(name)
-  puts "Hello(name)!"
-end
-
 def say_hello (name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
